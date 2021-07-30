b5a8abf3865fb3bf580f8c8fb663803847f95d42
5064 <5og4.kirin@gmail.com>
2021-07-30 15:34:45 +0900

    fix rotation bug and formatting

--- boid.js
+++ boid.js
@@ -1,116 +1,131 @@
 class Boid {
-    position;
-    velocity;
-    acceleration;
+  position;
+  velocity;
+  acceleration;
 
-    PERSEPTION_RADIUS = 70;
-    MAX_SPEED = 3;
-    MAX_FORCE = 0.1;
-    constructor(p5, pos, vel) {
-        this.p5 = p5;
-        this.position = this.p5.createVector(pos.x, pos.y);
-        this.velocity = this.p5.createVector(vel.x, vel.y);
-        this.acceleration = this.p5.createVector();
+  PERSEPTION_RADIUS = 70;
+  MAX_SPEED = 3;
+  MAX_FORCE = 0.1;
+  constructor(p5, pos, vel) {
+    this.p5 = p5;
+    this.position = this.p5.createVector(pos.x, pos.y);
+    this.velocity = this.p5.createVector(vel.x, vel.y);
+    this.acceleration = this.p5.createVector();
+  }
+
+  align = (boids) => {
+    let totalBoids = 0;
+    let average = this.p5.createVector();
+    for (let boid of boids) {
+      const d = this.p5.dist(
+        this.position.x,
+        this.position.y,
+        boid.position.x,
+        boid.position.y
+      );
+      if (d < this.PERSEPTION_RADIUS) {
+        average.add(boid.velocity);
+        totalBoids++;
+      }
+    }
+    if (totalBoids !== 0) {
+      average.div(totalBoids); // now got average around Boids's velocity
+      average.setMag(this.MAX_SPEED); // average is just direction, so unify magnitude
+      average.sub(this.velocity); // desired - current velocity = steering
+      average.limit(this.MAX_FORCE);
     }
 
-    align = (boids) => {
-        let totalBoids = 0;
-        let average = this.p5.createVector();
-        for (let boid of boids) {
-            const d = this.p5.dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (d < this.PERSEPTION_RADIUS) {
-                average.add(boid.velocity);
-                totalBoids++;
-            }
-        }
-        if (totalBoids !== 0) {
-            average.div(totalBoids);  // now got average around Boids's velocity
-            average.setMag(this.MAX_SPEED)  // average is just direction, so unify magnitude
-            average.sub(this.velocity);  // desired - current velocity = steering
-            average.limit(this.MAX_FORCE);
-        }
+    return average;
+  };
 
-        return average
+  cohesion = (boids) => {
+    let totalBoids = 0;
+    let average = this.p5.createVector();
+    for (let boid of boids) {
+      const d = this.p5.dist(
+        this.position.x,
+        this.position.y,
+        boid.position.x,
+        boid.position.y
+      );
+      if (d !== 0 && d < this.PERSEPTION_RADIUS) {
+        average.add(boid.position);
+        totalBoids++;
+      }
+    }
+    if (totalBoids !== 0) {
+      average.div(totalBoids); // now got average around Boids's position
+      average.sub(this.position);
+      average.setMag(this.MAX_SPEED);
+      average.sub(this.velocity); // average - current position
+      average.limit(this.MAX_FORCE);
     }
 
-    cohesion = (boids) => {
-        let totalBoids = 0;
-        let average = this.p5.createVector();
-        for (let boid of boids) {
-            const d = this.p5.dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (d !== 0 && d < this.PERSEPTION_RADIUS) {
-                average.add(boid.position);
-                totalBoids++;
-            }
-        }
-        if (totalBoids !== 0) {
-            average.div(totalBoids);  // now got average around Boids's position
-            average.sub(this.position);
-            average.setMag(this.MAX_SPEED);
-            average.sub(this.velocity);  // average - current position
-            average.limit(this.MAX_FORCE);
-        }
+    return average;
+  };
 
-        return average
+  separate = (boids) => {
+    let totalBoids = 0;
+    let average = this.p5.createVector();
+    for (let boid of boids) {
+      const d = this.p5.dist(
+        this.position.x,
+        this.position.y,
+        boid.position.x,
+        boid.position.y
+      );
+      if (d !== 0 && d < this.PERSEPTION_RADIUS / 2) {
+        let diff = this.p5
+          .createVector(this.position.x, this.position.y)
+          .sub(boid.position); // static method p5.Vector.sub()
+        diff.div(d);
+        average.add(diff);
+        totalBoids++;
+      }
     }
-
-    separate = (boids) => {
-        let totalBoids = 0;
-        let average = this.p5.createVector();
-        for (let boid of boids) {
-            const d = this.p5.dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (d !== 0 && d < this.PERSEPTION_RADIUS / 2) {
-                let diff = this.p5.createVector(this.position.x, this.position.y).sub(boid.position);  // static method p5.Vector.sub()
-                diff.div(d);
-                average.add(diff);
-                totalBoids++;
-            }
-        }
-        if (totalBoids !== 0) {
-            average.div(totalBoids);  // now got average around Boids's position
-            average.setMag(this.MAX_SPEED);
-            average.sub(this.velocity);  // average - current position
-            average.limit(this.MAX_FORCE);
-        }
-        return average
+    if (totalBoids !== 0) {
+      average.div(totalBoids); // now got average around Boids's position
+      average.setMag(this.MAX_SPEED);
+      average.sub(this.velocity); // average - current position
+      average.limit(this.MAX_FORCE);
     }
+    return average;
+  };
 
-    flockRule = (boids) => {
-        this.acceleration.mult(0);  // reset acceleration
-        this.acceleration.add(this.align(boids))
-        this.acceleration.add(this.cohesion(boids))
-        this.acceleration.add(this.separate(boids))
+  flockRule = (boids) => {
+    this.acceleration.mult(0); // reset acceleration
+    this.acceleration.add(this.align(boids));
+    this.acceleration.add(this.cohesion(boids));
+    this.acceleration.add(this.separate(boids));
+  };
+
+  update = () => {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+  };
+
+  edgeLoop = () => {
+    if (this.position.x > this.p5.width) {
+      this.position.x = 0;
     }
-
-    update = () => {
-        this.position.add(this.velocity);
-        this.velocity.add(this.acceleration);
+    if (this.position.y > this.p5.height) {
+      this.position.y = 0;
     }
-
-    edgeLoop = () => {
-        if (this.position.x > this.p5.width) {
-            this.position.x = 0
-        }
-        if (this.position.y > this.p5.height) {
-            this.position.y = 0
-        }
-        if (this.position.x < 0) {
-            this.position.x += this.p5.width
-        }
-        if (this.position.y < 0) {
-            this.position.y += this.p5.height
-        }
+    if (this.position.x < 0) {
+      this.position.x += this.p5.width;
     }
-
-    show = () => {
-        this.p5.push();
-        this.p5.translate(this.position.x, this.position.y);
-        this.p5.rotate(this.velocity.x, this.velocity.y);  // why drawing like fish...
-        this.p5.fill(this.p5.color(`hsl(200, 60%, 60%)`));
-        this.p5.quad(20, 0, 0, -7, -7, 0, 0, 7);
-        this.p5.pop();
+    if (this.position.y < 0) {
+      this.position.y += this.p5.height;
     }
+  };
 
+  show = () => {
+    this.p5.push();
+    this.p5.translate(this.position.x, this.position.y);
+    // this.p5.line(0, 0, this.velocity.x * 15, this.velocity.y * 15);
+    this.p5.rotate(Math.atan2(this.velocity.y, this.velocity.x));
+    this.p5.fill(this.p5.color(`hsl(200, 60%, 60%)`));
+    this.p5.quad(20, 0, 0, -7, -7, 0, 0, 7);
+    this.p5.pop();
+  };
 }
-
-
--- sketch.js
+++ sketch.js
@@ -1,35 +1,34 @@
 const Sketch = new p5((p5) => {
-    const CANVAS_X = 1000,
-        CANVAS_Y = 600;
+  const CANVAS_X = 1000,
+    CANVAS_Y = 600;
 
-    const MAX_NUM = 50;
-    const boids = [];
+  const MAX_NUM = 50;
+  const boids = [];
 
-    prepareBoids = () => {
-        for (let i = 0; i < MAX_NUM; i++) {
-            const randomRad = p5.TWO_PI * Math.random();
-            const b = new Boid(
-                p5,
-                { x: Math.random() * CANVAS_X, y: Math.random() * CANVAS_Y },
-                { x: Math.cos(randomRad) * 4, y: Math.sin(randomRad) * 4 }
-            )
-            boids.push(b);
-        }
+  prepareBoids = () => {
+    for (let i = 0; i < MAX_NUM; i++) {
+      const randomRad = p5.TWO_PI * Math.random();
+      const b = new Boid(
+        p5,
+        { x: Math.random() * CANVAS_X, y: Math.random() * CANVAS_Y },
+        { x: Math.cos(randomRad) * 4, y: Math.sin(randomRad) * 4 }
+      );
+      boids.push(b);
     }
+  };
 
-    p5.setup = () => {
-        p5.createCanvas(CANVAS_X, CANVAS_Y);
-        prepareBoids();
+  p5.setup = () => {
+    p5.createCanvas(CANVAS_X, CANVAS_Y);
+    prepareBoids();
+  };
+
+  p5.draw = () => {
+    p5.background(100);
+    for (let boid of boids) {
+      boid.edgeLoop();
+      boid.flockRule(boids);
+      boid.update();
+      boid.show();
     }
-
-    p5.draw = () => {
-        p5.background(100);
-        for (let boid of boids) {
-            boid.edgeLoop();
-            boid.flockRule(boids);
-            boid.update();
-            boid.show();
-        }
-    }
-})
-
+  };
+});
